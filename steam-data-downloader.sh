76a08197d3dc8ce02b45abb0ac294ff217c2cb18
Max Wofford <max@hackedu.us>
2015-03-22 06:02:57 +0000

    switch to GetOwnedGames endpoint

--- steam-data-downloader.sh
+++ steam-data-downloader.sh
@@ -1,1 +1,2 @@
-curl "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=$STEAM_API_KEY&steamid=$STEAM_ID&format=json" > steam-data/steam-data-$(date "+%Y_%m_%d".json)
+source ~/.bashrc
+curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_API_KEY&steamid=$STEAM_ID&format=json" > steam-data/steam-data-$(date "+%Y_%m_%d".json)
