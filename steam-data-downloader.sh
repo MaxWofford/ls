99baa69d23cef6943ed67ec4e29899b2ef08e72e
Max Wofford <max@hackedu.us>
2015-03-27 21:34:08 +0000

    Move steam-data/ to records/

--- steam-data-downloader.sh
+++ steam-data-downloader.sh
@@ -1,2 +1,2 @@
 source ~/.bashrc
-curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_API_KEY&steamid=$STEAM_ID&format=json" > steam-data/steam-data-$(date "+%Y_%m_%d".json)
+curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_API_KEY&steamid=$STEAM_ID&format=json" > "records/steam-data-$(date +%Y_%m_%d).json"
