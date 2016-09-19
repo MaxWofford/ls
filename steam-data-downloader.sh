30f6307f02c044eb4ad9e61a98cbcc73e55c54d1
Max Wofford <max@maxwofford.com>
2016-09-19 02:21:55 -0400

    Script is executable

--- steam-data-downloader.sh
+++ steam-data-downloader.sh
@@ -1,2 +1,3 @@
-source ~/.bashrc
+#!/bin/bash
+
 curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_API_KEY&steamid=$STEAM_ID&format=json" > "records/steam-data-$(date +%Y_%m_%d).json"
